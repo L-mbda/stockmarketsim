d2f3d68034988863a5cfd1fc73d67d86fd42f356
Defy <61808223+De-y@users.noreply.github.com>
2024-06-22 09:54:08 -0700

    alr, stonks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,42 @@ import useLocalStorage from "use-local-storage";
 export default function Home() {
   const [cash, setCash] = useLocalStorage("cash", 1000.00);
   const [localCurrency, setLocalCurrency] = useLocalStorage("currency", "USD");
-  const [stocks, setStocks] = useLocalStorage("stocks", []);
+  const [stocks, setStocks] = useLocalStorage("stocks", [{'ticker': 'Beginner', 'price': 1, 'quantity': 1}]);
 
-  fetch('/api/get_stocks', { method: 'POST', body: JSON.stringify({'ticker': 'AAPL'}) }).then(r => r.json().then(data => console.log(data)))
+  
+  function buyStock() {
+    // @ts-ignore
+    let ticker = document.getElementById('ticker-buy').value;
+    // @ts-ignore
+    let quantity = document.getElementById('quantity-buy').value;
+    let stockInfo;
+
+    fetch('/api/get_stocks', { method: 'POST', body: JSON.stringify({'ticker': ticker}) }).then(r => r.json().then(data => {
+      stockInfo = data.stock_information;
+      console.log(stockInfo);
+      let price = parseFloat(stockInfo['05. price']);
+      if ((price * quantity) > cash) {
+        alert(`Insufficent funds to buy ${quantity} of ${ticker} at ${price} individually for a total of ${price * quantity}`);
+        return;
+      }
+      setCash(cash - (price * quantity));
+      setStocks([stocks].push({'ticker': ticker, 'price': price, 'quantity': quantity}));
+      console.log(stocks);
+    }))
+  }
+  function searchStock() {
+    // @ts-ignore
+    let ticker = document.getElementById('ticker-search').value;
+    let stockInfo;
+    fetch('/api/get_stocks', { method: 'POST', body: JSON.stringify({'ticker': ticker}) }).then(r => r.json().then(data => {
+      stockInfo = data.stock_information;
+      console.log(stockInfo);
+      // @ts-ignore
+      document.getElementById('stock-search-ticker').innerText = stockInfo['01. symbol'];
+      // @ts-ignore
+      document.getElementById('stock-search-price').innerText = stockInfo['05. price'] + ' ' + localCurrency;
+    }))
+  }
   return (
     <>
       <div className='container'>
@@ -17,7 +50,31 @@ export default function Home() {
         </div>
         <div className='summative'>
           <h2>Stock Market</h2>
-          <p>Querying</p>
+          <div className='stocks'>
+            <p>Owned Stocks</p>
+          </div>
+          <div className='buy'>
+            <p>Search Stocks</p>
+            <input type='text' placeholder='Ticker' id='ticker-search'/>
+            <button onClick={searchStock}>Search</button>
+            <p>Stock Information</p>
+            <div className='stock'>
+              <p id="stock-search-ticker">Stock</p>
+              <p id="stock-search-price" className='price'>Price</p>
+            </div>
+          </div>
+          <div className='buy'>
+            <h1>Buy</h1>
+            <input type='text' placeholder='Ticker' id='ticker-buy'/>
+            <input type='number' placeholder='Quantity' id='quantity-buy'/>
+            <button onClick={buyStock}>Buy</button>
+          </div>
+          <div className='sell'>
+            <h1>Sell</h1>
+            <input type='text' placeholder='Ticker' id='ticker-sell'/>
+            <input type='number' placeholder='Quantity' id='quantity-sell'/>
+            <button>Sell</button>
+          </div>
         </div>
       </div>
     </>
