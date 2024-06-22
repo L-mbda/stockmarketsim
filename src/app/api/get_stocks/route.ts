54ae689bddfd4b83bb8a3f0527919ee9acf5b716
Defy <61808223+De-y@users.noreply.github.com>
2024-06-22 08:52:41 -0700

    DEAN

---
+++ src/app/api/get_stocks/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import axios from 'axios';
+
+export async function POST(request: any) {
+    let data = await request.json();
+    console.log(data);
+    let ticker = data.ticker;
+    let response = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&interval=5min&apikey=${process.env.AV_Code}`);
+    let stock_information = response.data['Global Quote'];
+    return NextResponse.json({ stock_information: stock_information });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,20 @@ import useLocalStorage from "use-local-storage";
 export default function Home() {
   const [cash, setCash] = useLocalStorage("cash", 1000.00);
   const [localCurrency, setLocalCurrency] = useLocalStorage("currency", "USD");
+  const [stocks, setStocks] = useLocalStorage("stocks", []);
+
+  fetch('/api/get_stocks', { method: 'POST', body: JSON.stringify({'ticker': 'AAPL'}) }).then(r => r.json().then(data => console.log(data)))
   return (
     <>
       <div className='container'>
-        <h1>Stock Market Simulator</h1>
-        <p>Current balance: {cash} {localCurrency}</p>
+        <div className='summative'>
+          <h1>Stock Market Simulator</h1>
+          <p>Current balance: {cash} {localCurrency}</p>
+        </div>
+        <div className='summative'>
+          <h2>Stock Market</h2>
+          <p>Querying</p>
+        </div>
       </div>
     </>
   );
